6241338fd1075af796a66036fe92042b970513ab
Cenyai <carlosyygg@gmail.com>
2023-05-04 18:56:03 +0200

    ajustando estilos para moviles

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
-document.getElementById("Back").addEventListener("click", function(){
-    window.location.href = "http://Unday/index.html"; // Reemplaza "index.html" con la URL de tu página principal
-  });
+window.onload = () => {
+    document.getElementById("Back").addEventListener("click", function () {
+        window.location.href = "https://cenyai.github.io/Unday/"; // Reemplaza "index.html" con la URL de tu página principal
+    });
+}
